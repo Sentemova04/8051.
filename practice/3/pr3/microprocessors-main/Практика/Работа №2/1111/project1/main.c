d36ebfd79a44473bfced98f82875b0089de61baf
Sentemova04 <133458312+Sentemova04@users.noreply.github.com>
2025-05-23 13:14:44 +0300

    Add files via upload

---
+++ practice/3/pr3/microprocessors-main/Практика/Работа №2/1111/project1/main.c
@@ -0,0 +1,39 @@
+#include <8051.h>
+
+void main(){
+	unsigned char i = 0, j = 0, a = 1;
+	unsigned char massiv [11] ={
+		0xC0,
+		0xF9,
+		0xA4,
+		0xB0,
+		0x99,
+		0x92,
+		0x82,
+		0xF8,
+		0x80,
+		0x90,
+		0xFF
+	};
+	P1 = massiv [10];
+	P3 = 0;
+	i=0;
+	
+	while (1){
+		while(P30==0){
+			P2 = massiv[i];
+			for(j=0;j<50;j++)
+				continue;
+			i+=1;
+			if (i>9) i=0;
+		}
+		while (P30==1){
+			P2 = massiv[i];
+			for(j=0;j<50;j++)
+				continue;
+			i+=2;
+			if (i>9) i=1;
+			
+		}
+	}
+}
